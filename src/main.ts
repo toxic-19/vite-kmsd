19646bdda67d7ef072d97b4375b9e68bc85ee3fd
toxic19 <3468528598@qq.com>
2024-03-30 13:07:21 +0800

    feat: 实现pinia的持久化存储

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import piniaPluginPersist from 'pinia-plugin-persist'
 import App from './App.vue'
 import globalComponent from '@/components/index' // 注册全局组件
 import 'virtual:svg-icons-register' // 注册 SVG 图标组件
@@ -7,6 +8,7 @@ import '@/styles/index.scss' // 导入公共样式
 import router from '@/router/index.ts' // 导入路由器
 const app = createApp(App)
 const pinia = createPinia()
+pinia.use(piniaPluginPersist) // 使用持久化存储插件
 app.use(globalComponent)
 app.use(router) // 路由挂载
 app.use(pinia) // 注册Pinia
--- src/stores/knowledge.ts
+++ src/stores/knowledge.ts
@@ -2,13 +2,22 @@ import { ref } from 'vue'
 import { defineStore } from 'pinia'
 
 // 设置在知识库页跳转到文档页时携带信息
-export const useKnowledgeStore = defineStore('knowledge', () => {
-  const currentKnowName = ref<string>('')
-  const currentKnowId = ref<number>(0)
-  function setCurrent(id, name) {
-    currentKnowId.value = id
-    currentKnowName.value = name
-  }
+export const useKnowledgeStore = defineStore(
+  'knowledge',
+  () => {
+    const currentKnowName = ref<string>('')
+    const currentKnowId = ref<number>(0)
+    function setCurrent(id, name) {
+      currentKnowId.value = id
+      currentKnowName.value = name
+    }
 
-  return { currentKnowId, currentKnowName, setCurrent } // 组合式写法必须返回state中的所有属性
-})
+    return { currentKnowId, currentKnowName, setCurrent } // 组合式写法必须返回state中的所有属性
+  },
+  {
+    persist: {
+      enabled: true,
+      strategies: [{ storage: sessionStorage }],
+    },
+  },
+)
